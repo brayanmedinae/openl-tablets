e15a4ba132f44f855a2bb55f07ad39051fe4884a
Brayan Medina <bmedina@stiglobals.com>
2024-09-26 12:09:42 -0600

    Initial commit

---
+++ src/main/java/sti/globals/App.java
@@ -0,0 +1,13 @@
+package sti.globals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
